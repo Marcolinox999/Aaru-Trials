c3fcd9c49f0cd872d9ceb55f48b57d54be580f1c
Alejandro_Martin_del_Rey <alejandro.del@live.u-tad.com>
2026-05-13 00:21:57 +0200

    Teleports

--- Assets/MenuScripts/TeleportScript.cs
+++ Assets/MenuScripts/TeleportScript.cs
@@ -3,23 +3,30 @@ using UnityEngine.SceneManagement;
 
 public class TeleportScript : MonoBehaviour
 {
+    [Header("DESTROY")]
     [SerializeField] private GameObject BlockPortal2;
     [SerializeField] private GameObject BlockPortal3;
     [SerializeField] private GameObject BlockPortalFinalBoss;
     
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player") && gameObject.CompareTag("Trial1"))
+        if (other.gameObject.CompareTag("Player") && gameObject.CompareTag("Trial1") )
         {
-            SceneManager.LoadScene("Scenes/Trial_1");
+            SceneManager.LoadScene("Scenes/Trial_1"); 
+            if (BlockPortal2 != null)
+                Destroy(BlockPortal2);
         }
         else if (other.gameObject.CompareTag("Player") && gameObject.CompareTag("Trial2"))
         {
             SceneManager.LoadScene("Scenes/Trial_2");
+            if (BlockPortal3 != null)
+                Destroy(BlockPortal3);
         }
         else if (other.gameObject.CompareTag("Player") && gameObject.CompareTag("Trial3"))
         {
             SceneManager.LoadScene("Scenes/Trial_3");
+            if (BlockPortalFinalBoss != null)
+                Destroy(BlockPortalFinalBoss);
         }
         else if (other.gameObject.CompareTag("Player") && gameObject.CompareTag("FinalBoss"))
         {
