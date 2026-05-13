7641967fe1310704c903caf37ffc7442a9e3a28e
Alejandro_Martin_del_Rey <alejandro.del@live.u-tad.com>
2026-05-13 17:19:41 +0200

    Player teleport and fuctional movement between levels

---
+++ Assets/MenuScripts/PlayerDataInfo.cs
@@ -0,0 +1,8 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDataInfo : MonoBehaviour
+{
+   public int levelsPassed = 0;
+}
--- Assets/MenuScripts/TeleportScript.cs
+++ Assets/MenuScripts/TeleportScript.cs
@@ -1,32 +1,61 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TeleportScript : MonoBehaviour
 {
-    [Header("DESTROY")]
+    private PlayerDataInfo playerStoredInfo;
+    
     [SerializeField] private GameObject BlockPortal2;
     [SerializeField] private GameObject BlockPortal3;
     [SerializeField] private GameObject BlockPortalFinalBoss;
-    
+    private void Awake()
+    {
+        playerStoredInfo = GameObject.FindGameObjectWithTag("Player")?.GetComponent<PlayerDataInfo>();
+
+        switch (playerStoredInfo.levelsPassed)
+        {
+            case 1:
+                BlockPortal2.SetActive(false);
+                return;
+            case 2:
+                BlockPortal2.SetActive(false);
+                BlockPortal3.SetActive(false);
+                return;
+            case 3:
+                BlockPortal2.SetActive(false);
+                BlockPortal3.SetActive(false);
+                BlockPortalFinalBoss.SetActive(false);
+                return;
+                
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player") && gameObject.CompareTag("Trial1") )
         {
-            SceneManager.LoadScene("Scenes/Trial_1"); 
-            if (BlockPortal2 != null)
-                Destroy(BlockPortal2);
+            SceneManager.LoadScene("Scenes/Trial_1");
+            if (playerStoredInfo.levelsPassed <= 0)
+            {
+                playerStoredInfo.levelsPassed = 1;
+            }
         }
         else if (other.gameObject.CompareTag("Player") && gameObject.CompareTag("Trial2"))
         {
             SceneManager.LoadScene("Scenes/Trial_2");
-            if (BlockPortal3 != null)
-                Destroy(BlockPortal3);
+            if (playerStoredInfo.levelsPassed <= 1)
+            {
+                playerStoredInfo.levelsPassed = 2;
+            }
         }
         else if (other.gameObject.CompareTag("Player") && gameObject.CompareTag("Trial3"))
         {
             SceneManager.LoadScene("Scenes/Trial_3");
-            if (BlockPortalFinalBoss != null)
-                Destroy(BlockPortalFinalBoss);
+            if (playerStoredInfo.levelsPassed <= 2)
+            {
+                playerStoredInfo.levelsPassed = 3;
+            }
         }
         else if (other.gameObject.CompareTag("Player") && gameObject.CompareTag("FinalBoss"))
         {
