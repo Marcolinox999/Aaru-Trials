851f988d7fa4e25963428a5299cee85695a6a0f0
Marcolinox999 <Marcolinox999@gmail.com>
2026-05-13 05:46:18 +0200

    Last fixes to powers, and attack system for the enemies and life for the player

--- Assets/Scripts/Enemies/EnemyAttacks(PROVISIONAL).cs
+++ Assets/Scripts/Enemies/EnemyAttacks(PROVISIONAL).cs
@@ -5,24 +5,58 @@ using UnityEngine;
 public class EnemyAttacks : MonoBehaviour
 {
   private bool canAttack = true;
-  [SerializeField] PlayerLife life;
-  private void OnTriggerStay(Collider other)
+  [SerializeField]  private Animator animator;
+  [SerializeField] private PlayerLife life;
+  [SerializeField] private Material _materialAttack;
+  [SerializeField] private LayerMask playerMask;
+  private Renderer _renderer;
+  private Material _materialDefault;
+  private PlayerLife _player;
+  [SerializeField] private float radius = 0.75f;
+  [SerializeField] private float range = 2f;
+
+  private void Start()
+  {
+    _renderer = GetComponentInChildren<Renderer>();
+    _materialDefault =  _renderer.material;
+  }
+
+  private void OnTriggerEnter(Collider other)
   {
     if (other.gameObject.CompareTag("Player"))
       if (canAttack)
       {
-        StartCoroutine(CoolDown());
-        life.TakeDamage(10);
+        _player = other.gameObject.GetComponent<PlayerLife>();
+        canAttack = false;
+        StartCoroutine(AttackItSelf());
+        //life.TakeDamage(10);
       }
   }
-
-  IEnumerator  CoolDown()
+  
+  IEnumerator  AttackItSelf()
   {
-    canAttack = false;
+    Material[] mats = _renderer.materials;
+    mats[1] = _materialAttack;
+    _renderer.materials = mats;
+    animator.Play("attack");
+    _player.TakeDamage(10);
+    yield return new WaitForSeconds(1f);
+    mats[1] = _materialDefault;            
+    _renderer.materials = mats;           
     Debug.Log("Cooling Down");
     yield return new WaitForSeconds(3f);
     Debug.Log("Cooling Up");
     
     canAttack = true;
+  }
+  private void OnDrawGizmosSelected()
+  {
+    Gizmos.color = Color.red;
+    Vector3 origin = transform.position;
+    Vector3 direction = transform.forward;
+    Gizmos.DrawWireSphere(origin, radius);
+    Vector3 end = origin + direction * range;
+    Gizmos.DrawWireSphere(end, radius);
+    Gizmos.DrawLine(origin, end);
   }
 }
--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -18,6 +18,6 @@ public class PlayerLife : MonoBehaviour
     public void TakeDamage(float damage)
     {
         life -= damage;
-        lifebar.value = life / 100;
+        lifebar.value = life;
     }
 }
