ff3faa39262d1eaade1c8ef689f8b020725f55e9
Alejandro_Martin_del_Rey <alejandro.del@live.u-tad.com>
2026-05-28 07:16:02 +0200

    UI full fix (missing Death)

--- Assets/MenuScripts/MainMenu.cs
+++ Assets/MenuScripts/MainMenu.cs
@@ -4,6 +4,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private GameObject ui;
+    public void Awake()
+    {
+        ui = GameObject.FindGameObjectWithTag("UI");
+        if (ui != null)
+        {
+            Destroy(ui);
+        }
+       
+    }
     public void NewGame()
     {
         SceneManager.LoadScene(1);
