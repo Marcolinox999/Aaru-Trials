2f22a643dff3e8ba87836d7ecf67b4d24c044de1
Alejandro_Martin_del_Rey <alejandro.del@live.u-tad.com>
2026-05-28 14:29:15 +0200

    Ui

--- Assets/MenuScripts/StageCompleted.cs
+++ Assets/MenuScripts/StageCompleted.cs
@@ -1,20 +1,58 @@
 using System;
+using System.Net.Mime;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class StageCompleted : MonoBehaviour
 {
     private GameObject enemiesInLevel;
     private EnemyManager manager;
-    [SerializeField] PauseMenu pauseMenu;
     
+    [SerializeField] PauseMenu pauseMenu;
+    [SerializeField] TMP_Text ability1;
+    [SerializeField] TMP_Text  ability2;
 
+    private GameObject slot1;
+    private GameObject slot2;
+
+    public bool swap;
+    
+    public GameObject grappling;
+    public GameObject kopesh;
+    public GameObject healing;
+    public GameObject iceExplosion;
+    public GameObject venomLauncher;
+    public GameObject rockLaunch;
+    public GameObject knifes;
+
+    [SerializeField] public GameObject[] abilities;
+    
+    public int rnd;
+    public int rnd2;
+
+    private bool abilitiesPicked;
+    
     private void NewSceneLoaded()
     {
         enemiesInLevel = GameObject.FindGameObjectWithTag("EnemyManager");
         if (enemiesInLevel != null)
         {
+            rnd = Random.Range(0, abilities.Length);
+            rnd2 = Random.Range(0, abilities.Length);
+            while (rnd == rnd2)
+            {
+                rnd2 = Random.Range(0, abilities.Length);
+            }
+            ability1.text = abilities[rnd].name;
+            ability2.text = abilities[rnd2].name;
+            
             manager = enemiesInLevel.GetComponent<EnemyManager>();
+            
+            abilitiesPicked = true;
         }
     }
 
@@ -22,7 +60,10 @@ public class StageCompleted : MonoBehaviour
     {
         if (SceneManager.GetActiveScene().name != "MainHall")
         {
-            NewSceneLoaded();
+            if (abilitiesPicked == false)
+            {
+                NewSceneLoaded();
+            }
 
             if (enemiesInLevel != null)
             {
@@ -38,19 +79,58 @@ public class StageCompleted : MonoBehaviour
     public void Ability_1()
     {
         Unfreze();
+        AbilitiesRemaining1();
         SceneManager.LoadScene("MainHall");
     }
 
     public void Ability_2()
     {
         Unfreze();
+        AbilitiesRemaining2();
         SceneManager.LoadScene("MainHall");
+    }
+
+    private void AbilitiesRemaining1()
+    {
+        if (slot1 == null)
+        {
+            slot1 = abilities[rnd];
+            abilities[rnd].SetActive(true);
+        }
+        else if (slot2 == null)
+        {
+            slot2 = abilities[rnd];
+            abilities[rnd].SetActive(true);
+        }
+        else
+        {
+            swap = true;
+        }
+    }
+    private void AbilitiesRemaining2()
+    {
+        if (slot1 == null)
+        {
+            slot1 = abilities[rnd2];
+            abilities[rnd2].SetActive(true);
+        }
+        else if (slot2 == null)
+        {
+            slot2 = abilities[rnd2];
+            abilities[rnd2].SetActive(true);
+        }
+        else
+        {
+            swap = true;
+        }
     }
     
     private void Unfreze()
     {
         if (enemiesInLevel != null)
         {
+            
+            abilitiesPicked = false;
             pauseMenu.enabled = true;
             manager.stageCompleted = false;
             
